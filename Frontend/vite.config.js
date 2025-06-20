82152f345185b3b1b0f1702ed66895ae19463bdb
Amarjeet <amarjeetofficial81@gmail.com>
2025-06-20 14:04:02 +0530

    update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://drawwify-in-backend.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
