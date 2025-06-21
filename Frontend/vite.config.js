9661c3eee479220983277ec83e4ecf7ec7a27d5a
Amarjeet <amarjeetofficial81@gmail.com>
2025-06-21 11:47:54 +0530

    change the structure of vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,20 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_BACKEND_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, path.resolve(__dirname, '../'), '');
+  return {
+    base: '/',
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
       }
     }
   }
