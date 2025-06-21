2bde72f2f67cdcc5121b802f5e489ecd0e200f13
Amarjeet <amarjeetofficial81@gmail.com>
2025-06-21 11:49:10 +0530

    import fileURLToPath

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
